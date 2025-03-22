0cab3cbc808ca49544271f8a7770ec113ba93612
praveenchalla <sreepraveenchalla@gmail.com>
2025-03-22 16:46:15 +0800

    created databse schemas and wntities and make perfect connection between table

--- src/main/java/com/praveen/moviebasicapplication/MovieService.java
+++ src/main/java/com/praveen/moviebasicapplication/MovieService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MovieService {
     @Autowired
